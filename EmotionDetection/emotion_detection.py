1ab0f54689be917f94d8fe9756881f6e1642fbbb
MaduraDias <diasmadura@yahoo.co.uk>
2025-04-27 02:23:33 -0400

    Refactor the emotion_detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -21,22 +21,14 @@ def emotion_detector(text_to_analyze):
         # Make the POST request to the API
         response = requests.post(api_url, json=payload, headers=headers)
 
-  # Check for 400 Bad Request
-        if response.status_code == 400:
-            error_res = {
-                "anger": 'None', 
-                "disgust": 'None', 
-                "fear": 'None', 
-                "joy": 'None', 
-                "sadness": 'None', 
-                "dominant_emotion":'None'
-                }
-            return  error_res
-
-        # Raise an exception if the status code indicates an error
+         # Raise an exception if the status code indicates an error
         response.raise_for_status()
 
         emotions_dictionary = response.json()['emotionPredictions'][0]['emotion']
+        
+        #find dominant emotion and add too the dictionary
+        dominant_emotion = max(emotions_dictionary, key=emotions_dictionary.get)
+        emotions_dictionary['dominant_emotion'] = dominant_emotion
 
         return emotions_dictionary
 
