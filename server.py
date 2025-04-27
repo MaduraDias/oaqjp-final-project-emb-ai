d067c4195f17d86f8685d81f11b4fe2833aa8055
MaduraDias <diasmadura@yahoo.co.uk>
2025-04-27 06:51:03 -0400

    Fixing lint errors in server.py

--- server.py
+++ server.py
@@ -5,14 +5,12 @@ app = Flask("Emotion Detector App")
 
 @app.route('/emotionDetector')
 def get_emotion():
-    textToAnalyze = request.args.get('textToAnalyze')
-    result = emotion_detector(textToAnalyze)
+    text_to_analyze = request.args.get('textToAnalyze')
+    result = emotion_detector(text_to_analyze)
 
     if result["dominant_emotion"] == "None":
-       return(" Invalid text! Please try again!.", 400)
-       
-    return (result)
-    
+        return(" Invalid text! Please try again!.", 400)
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
