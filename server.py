fcbd8d39fc13055b8387b9a78fd2863483a9dc07
MaduraDias <diasmadura@yahoo.co.uk>
2025-04-26 20:48:58 -0400

    Adding flask app

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector App")
+
+@app.route('/emotionDetector')
+def get_emotion():
+    textToAnalyze = request.args.get('textToAnalyze')
+    result = emotion_detector(textToAnalyze)
+    return (result)
+    
+
+if __name__ == "__main__":
+    app.run(debug=true)
